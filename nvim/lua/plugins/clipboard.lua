f02986bc79fda6b451ba50b2bd33cd026ddb08cf
Mo Omer <mo@ipv7.com>
2026-01-18 13:06:24 -0600

    enable vim clipboard

---
+++ nvim/lua/plugins/clipboard.lua
@@ -0,0 +1,15 @@
+-- OSC 52 clipboard support (works through tmux/SSH/WSL)
+-- Requires neovim 0.10+
+if vim.fn.has('nvim-0.10') == 1 then
+  vim.g.clipboard = {
+    name = 'OSC 52',
+    copy = {
+      ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+      ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+    },
+    paste = {
+      ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
+      ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
+    },
+  }
+end
